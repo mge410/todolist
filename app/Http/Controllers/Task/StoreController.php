be38349136859e537d71a0566b9e774eaf3ffa65
Vladimir <v0v.voron2005@yandex.ru>
2023-06-13 22:36:32 +0300

    Update StoreController task

--- app/Http/Controllers/Task/StoreController.php
+++ app/Http/Controllers/Task/StoreController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers\Task;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Task\StoreRequest;
-use App\Models\Task;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request, int $listId)
     {
         $data = $request->validated();
         $data['list_id'] = $listId;
-        $task = Task::Create($data);
+
+        $task = $this->service->store($data);
+
         return response()->json([
             'success' => 'Task created successfully',
             'data' => $task
