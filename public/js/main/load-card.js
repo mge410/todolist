1f99f722a24e232f9ef46f69f4a1f2730e0b5904
Vladimir <v0v.voron2005@yandex.ru>
2023-06-13 15:43:43 +0300

    Update index page for todolist print

--- public/js/main/load-card.js
+++ public/js/main/load-card.js
@@ -4,14 +4,16 @@ function displayData(data) {
     const cardContent = `
         <div class="card m-1 card-item" id="card-${data.id}" data-id="${data.id}">
             <div class="card-body">
-                <h5 class="card-title" id="titleContent-${data.id}">${data.title}</h5>
+            <a href="/main/${data.id}/task/">
+               <h5 class="card-title" id="titleContent-${data.id}">${data.title}</h5>
+            </a>
                 <p class="card-text" id="descriptionContent-${data.id}">${data.description}</p>
                 <p class="card-text"><small class="text-muted">The last task was <span class="text-danger-emphasis">3 mins ago</span></small></p>
                 <a data-bs-toggle="modal"
                            data-bs-target="#exampleModalEdit-${data.id}">
                             <i class="bi link bi-pencil-fill text-primary m-1"
                                id="delete-list"></i>
-                        </a>                <a class="btn-delete-list" data-route="main/destroy/${data.id}" data-id="${data.id}">
+                        </a>                <a class="btn-delete-list" data-route="/main/destroy/${data.id}" data-id="${data.id}">
                     <i class="bi link bi-trash-fill text-danger m-1" id="delete-list"></i>
                 </a>
             </div>
